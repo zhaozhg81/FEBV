4ed70898147214802be49e2eb8f4761696b78b7f
Zhigen Zhao <zhaozhg@guoliang.guoliang>
2021-05-12 22:58:37 -0400

    update

--- simulation/BatchVarComp.sh
+++
@@ -1,9 +0,0 @@
- R CMD BATCH VarComp_1.R &
-## R CMD BATCH VarComp_2.R &
-## R CMD BATCH VarComp_3.R &
-## R CMD BATCH VarComp_4.R &
-
-## R CMD BATCH VarComp_5.R &
-## R CMD BATCH VarComp_6.R &
-## R CMD BATCH VarComp_7.R &
-## R CMD BATCH VarComp_8.R &
